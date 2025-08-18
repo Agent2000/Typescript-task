f5228a6cda9d7240deee72e721c0471a504d9217
Антон Некрасов <nas_box@rambler.ru>
2025-08-18 21:04:19 +0300

    calc price

--- src/price.ts
+++ src/price.ts
@@ -1,7 +1,15 @@
-const totalPrice = (): number => {
-  return 42;
-};
+interface IPrice {
+  price: number;
+  discount: number;
+  isInstallment: boolean;
+  mounts: number;
+}
 
-const price = totalPrice();
+const totalPrice = (data: IPrice): number => {
+  return (data.price - (data.price * data.discount) / 100) / data.mounts;
+};
+const price1: IPrice = { price: 100000, discount: 25, isInstallment: true, mounts: 12 };
+
+const price: number = totalPrice(price1);
 
 console.log(price);
